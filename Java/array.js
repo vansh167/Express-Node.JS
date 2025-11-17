15de243ab19e3ff9378ea02e7cb250a1c4fc29ff
Vansh <vanshdoc7@gmail.com>
2025-11-17 12:15:17 +0530

    Arrays

--- Java/array.js
+++ Java/array.js
@@ -1,5 +1,20 @@
-let fruits = ["apple", "banana", "orange"];
-fruits.push("mango")  //add at end
-fruits.pop(); //removes last item
+// let fruits = ["apple", "banana", "orange"];
+// fruits.push("mango")  //add at end
+// fruits.pop(); //removes last item
 
-console.log(fruits [0]); // for access
+// console.log(fruits [0]); // for access
+
+
+
+// -------------------------Object---------------------------
+
+let person = {
+    name : "vansh",
+    age : "20",
+    city: "Una H.P"
+}
+
+console.log(person.name);
+console.log(person["age"]);
+
+module.exports = {person}
--- Java/loops.js
+++ Java/loops.js
@@ -1,8 +1,12 @@
 
-const math = require('./math');
+// const math = require('./math');
 
 // console.log("Your answer is :",2+3);
 // console.log("Your answer is :",2-2);
+
+const array = require('./array')
+
+console.log(person.name);
 
 
 
